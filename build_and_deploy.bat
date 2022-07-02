8ef44fb840b5b3f1ee094af54eeb7cc168e5b55e
Kodey-Kayla <tylerkodey@me.com>
2022-07-01 19:12:15 -0700

    Made build script work for users other than matth

--- build_and_deploy.bat
+++ build_and_deploy.bat
@@ -1,5 +1,5 @@
 "C:\Program Files\7-Zip\7z.exe" a -tzip "DoNotPullAWirtual.op" "*"
-del "C:\Users\matth\OpenplanetNext\Plugins\DoNotPullAWirtual.op"
-move "DoNotPullAWirtual.op" "C:\Users\matth\OpenplanetNext\Plugins\DoNotPullAWirtual.op"
+del "%userprofile%\OpenplanetNext\Plugins\DoNotPullAWirtual.op"
+move "DoNotPullAWirtual.op" "%userprofile%\OpenplanetNext\Plugins\DoNotPullAWirtual.op"
 
-:: Get-Content -Path "C:\Users\matth\OpenplanetNext\Openplanet.log" -Wait
+:: Get-Content -Path "%userprofile%\OpenplanetNext\Openplanet.log" -Wait
